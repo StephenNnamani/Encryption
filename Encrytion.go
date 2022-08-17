3ca6ec5bfbd61066ab31d55641281cf2634f67a9
StephenNnamani <sir.stephennnamani@gmail.com>
2022-08-17 14:27:30 +0100

    Update Encrytion.go

--- Encrytion.go
+++ Encrytion.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 )
 
@@ -10,12 +11,21 @@ var alphabets = []byte("a b c d e f g h i j k l m n o p q r s t u v w x y z")
 // Please keep this key secret
 var secretKey string = "£ * % & > < ! ) \" ( @ a b c d e f g h i j k l m n o"
 
-func main() {
-	fmt.Println("Hello")
+// Encoding here
+func encodeBase64(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
 }
 
-func encrypt() {
-	fmt.Println("encrypt here")
+//Decoding here
+func decodeBase64(s string) []byte {
+	data, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	fmt.Println("Hello")
 }
 
 func decrypt() {
