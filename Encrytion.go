f165217bbc17d1276bef2d994581ac30278f4906
StephenNnamani <sir.stephennnamani@gmail.com>
2022-08-17 14:16:05 +0100

    started

---
+++ Encrytion.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Initialized Vector or Alphabets
+var alphabets = []byte("a b c d e f g h i j k l m n o p q r s t u v w x y z")
+
+// Please keep this key secret
+var secretKey string = "£ * % & > < ! ) \" ( @ a b c d e f g h i j k l m n o"
+
+func main() {
+	fmt.Println("Hello")
+}
+
+func encrypt() {
+	fmt.Println("encrypt here")
+}
+
+func decrypt() {
+	fmt.Print("Decrypt here")
+}
